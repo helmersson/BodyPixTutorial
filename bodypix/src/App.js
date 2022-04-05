3e59fe7603ac764b9849825ee50d6278894b686c
helmersson <50464822+helmersson@users.noreply.github.com>
2022-04-05 10:56:11 +0200

    second init

--- bodypix/src/App.js
+++ bodypix/src/App.js
@@ -1,10 +1,9 @@
-// **TODO
 // 1. Install dependencies DONE
 // 2. Import dependencies DONE
 // 3. Setup webcam and canvas DONE
 // 4. Define refeerences to those DONE
-// 5. Load boxypix
-// 6. Detect function
+// 5. Load boxypix DONE
+// 6. Detect function DONE
 // 7. Draw using drawMask
 
 import React, { useRef } from "react";
@@ -21,6 +20,52 @@ function App() {
     const runBodysegment = async () => {
         const net = await bodyPix.load();
         console.log("BodyPix model loaded successfully");
+        setInterval(() => {
+            detect(net);
+        }, 100);
+    };
+
+    const detect = async (net) => {
+        // Check data is available
+        if (
+            typeof webcamRef.current !== "undefined" &&
+            webcamRef.current !== null &&
+            webcamRef.current.video.readyState === 4
+        ) {
+            // Get video properties
+            const video = webcamRef.current.video;
+            const videoHeight = webcamRef.current.video.videoHeight;
+            const videoWidth = webcamRef.current.video.videoWidth;
+
+            // Set video width and height
+            webcamRef.current.video.width = videoWidth;
+            webcamRef.current.video.height = videoHeight;
+
+            // Set canvas width and height
+            canvasRef.current.width = videoWidth;
+            canvasRef.current.height = videoHeight;
+
+            // Make detections
+            // * One of (see documentation below):
+            // *   - net.segmentPerson
+            // *   - net.segmentPersonParts
+            // *   - net.segmentMultiPerson
+            // *   - net.segmentMultiPersonParts
+            const person = await net.segmentPersonParts(video);
+            console.log(person);
+
+            // Draw detections
+            const coloredPartImage = bodyPix.toColoredPartMask(person);
+
+            bodyPix.drawMask(
+                canvasRef.current,
+                video,
+                coloredPartImage,
+                0.7,
+                0,
+                false
+            );
+        }
     };
 
     runBodysegment();
